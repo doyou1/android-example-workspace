49b44b3197a55dda88d7501a1f567a0672006862
doyou1 <56281493+doyou1@users.noreply.github.com>
2022-02-03 22:36:59 +0900

    add qnadetail layout design

--- QnaProject/app/src/main/java/com/example/qnaproject/QnaDetailActivity.kt
+++ QnaProject/app/src/main/java/com/example/qnaproject/QnaDetailActivity.kt
@@ -1,7 +1,9 @@
 package com.example.qnaproject
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,10 +16,21 @@ class QnaDetailActivity: AppCompatActivity() {
         setContentView(R.layout.activity_qna_detail)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+        val mActivity = this as QnaDetailActivity
+        findViewById<ImageButton>(R.id.tb_back).setOnClickListener(
+            View.OnClickListener {
+                val intent = Intent(this, QnaActivity::class.java)
+
+                mActivity.startActivity(intent)
+                mActivity.finish()
+            }
+        )
+
+
         // Get Intent Extra
         if(intent.hasExtra("QNA_ID")) {
             val qna_id = intent.getIntExtra("QNA_ID", -1)
-            findViewById<TextView>(R.id.tv_test).text = qna_id.toString()
+//            findViewById<TextView>(R.id.tv_test).text = qna_id.toString()
         }
     }
 }
