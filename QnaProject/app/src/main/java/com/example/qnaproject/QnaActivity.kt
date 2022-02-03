d395b3f4d89b2beffb7c80bb3f7e50915f655366
doyou1 <56281493+doyou1@users.noreply.github.com>
2022-02-03 21:03:03 +0900

    add toolbar

--- QnaProject/app/src/main/java/com/example/qnaproject/QnaActivity.kt
+++ QnaProject/app/src/main/java/com/example/qnaproject/QnaActivity.kt
@@ -7,5 +7,6 @@ class QnaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qna)
+        setSupportActionBar(findViewById(R.id.toolbar))
     }
 }
