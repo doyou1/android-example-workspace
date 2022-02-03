92902fdb0f5a0a565eaed81ea09a48e0b0ca07a5
doyou1 <56281493+doyou1@users.noreply.github.com>
2022-02-03 20:49:05 +0900

    QnaProject init

---
+++ app/src/main/java/com/example/qnaproject/QnaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.qnaproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QnaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qna)
+    }
+}
