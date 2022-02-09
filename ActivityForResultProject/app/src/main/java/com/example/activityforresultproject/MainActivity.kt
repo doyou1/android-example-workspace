6347eb874b96663f5a1b9c0aeaeff673a136a7ad
doyou1 <tla149@daum.net>
2022-02-09 11:57:22 +0900

    access gallery

--- ActivityForResultProject/app/src/main/java/com/example/activityforresultproject/MainActivity.kt
+++ ActivityForResultProject/app/src/main/java/com/example/activityforresultproject/MainActivity.kt
@@ -4,20 +4,27 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+
+    private val tag = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val launcher = getSubActivityResultLauncher()
+        val subLauncher = getSubActivityResultLauncher()
+        val galleryLauncher = getGalleryActivityResultLauncher()
 
-        findViewById<Button>(R.id.btn_move).setOnClickListener {
-            openSubActivityResultLauncher(launcher)
+        findViewById<Button>(R.id.btn_sub_move).setOnClickListener {
+            openSubActivityResultLauncher(subLauncher)
+        }
+        findViewById<Button>(R.id.btn_gallery_move).setOnClickListener {
+            openGalleryActivityResultLauncher(galleryLauncher)
         }
     }
 
@@ -36,7 +43,33 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun getGalleryActivityResultLauncher() : ActivityResultLauncher<Intent> {
+        return registerForActivityResult(
+            ActivityResultContracts
+                .StartActivityForResult()) { result ->
+
+            if (result.resultCode == Activity.RESULT_OK) {
+                // There are no request codes
+                val data: Intent? = result.data
+
+                data?.let { data ->
+                    val selectedImage = data.data
+                    Log.e(tag, selectedImage.toString())
+                }
+            }
+        }
+    }
+
     private fun openSubActivityResultLauncher(launcher: ActivityResultLauncher<Intent>) {
         launcher.launch(Intent(this, SubActivity::class.java))
+    }
+
+    private fun openGalleryActivityResultLauncher(launcher: ActivityResultLauncher<Intent>) {
+        val intent = Intent(Intent.ACTION_PICK).also {
+            it.setType("image/*")
+            val mimeTypes = arrayOf("image/jpg")
+            it.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
+        }
+        launcher.launch(intent)
     }
 }
