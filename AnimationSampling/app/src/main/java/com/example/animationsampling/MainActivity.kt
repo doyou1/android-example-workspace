e4b8605da032a920a89d5c34c5304ee0298b7851
doyou1 <tla149@daum.net>
2023-01-15 16:55:45 +0900

    update animation sampling (rotation, alpha, translation)

--- AnimationSampling/app/src/main/java/com/example/animationsampling/MainActivity.kt
+++ AnimationSampling/app/src/main/java/com/example/animationsampling/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.example.animationsampling
 
-import android.R
 import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.graphics.translationMatrix
-import androidx.dynamicanimation.animation.DynamicAnimation
-import androidx.dynamicanimation.animation.FlingAnimation
+import androidx.core.animation.doOnEnd
 import com.example.animationsampling.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val DURATION = 3000L
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,20 +21,44 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-
         setClickEvent()
     }
 
     private fun setClickEvent() {
-        binding.btn.setOnClickListener {
-            doAnimation()
+        binding.btnRotate.setOnClickListener {
+            doRotate()
+        }
+        binding.btnAlpha.setOnClickListener {
+            doAlpha()
+        }
+        binding.btnTranslation.setOnClickListener {
+            doTranslation()
         }
     }
 
-    private fun doAnimation() {
-        ObjectAnimator.ofFloat(binding.btn, "alpha", 1f, 0.5f, 0f).apply {
-            duration = 2000
+    private fun doRotate() {
+        ObjectAnimator.ofFloat(binding.btnRotate, "rotation", 0f, 360f).apply {
+            duration = DURATION
             start()
+        }
+    }
+
+    private fun doAlpha() {
+        ObjectAnimator.ofFloat(binding.btnAlpha, "alpha", 1f, 0.5f, 0f, 0.5f, 1f).apply {
+            duration = DURATION
+            start()
+        }
+    }
+
+    private fun doTranslation() {
+        ObjectAnimator.ofFloat(binding.btnTranslation, "translationX", 0f, 100f, 0f).apply {
+            duration = DURATION
+            start()
+        }.doOnEnd {
+            ObjectAnimator.ofFloat(binding.btnTranslation, "translationY", 0f, 100f, 0f).apply {
+                duration = DURATION
+                start()
+            }
         }
     }
 
