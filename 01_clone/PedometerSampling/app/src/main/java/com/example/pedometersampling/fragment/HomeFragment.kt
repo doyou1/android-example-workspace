decc4cb8a463d1fc95fe4fa90534a4a617434fa2
doyou1 <tla149@daum.net>
2023-02-12 15:34:09 +0900

    add ChartUtil

--- 01_clone/PedometerSampling/app/src/main/java/com/example/pedometersampling/fragment/HomeFragment.kt
+++ 01_clone/PedometerSampling/app/src/main/java/com/example/pedometersampling/fragment/HomeFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import com.example.pedometersampling.databinding.FragmentHomeBinding
 import com.example.pedometersampling.room.Pedometer
+import com.example.pedometersampling.util.ChartUtil
 import com.example.pedometersampling.util.GOAL_STEPS
 import com.example.pedometersampling.util.Util
 import com.github.mikephil.charting.data.PieData
@@ -50,25 +51,8 @@ class HomeFragment : BaseFragment() {
         binding.chart.holeRadius = 45f
         binding.chart.transparentCircleRadius = 50f
         binding.chart.legend.isEnabled = false
-        binding.chart.data = generateData(item)
-
+        binding.chart.data = ChartUtil.generatePieData(item)
         binding.chart.invalidate()
-    }
-
-    private fun generateData(item: Pedometer?): PieData {
-        val entries = arrayListOf<PieEntry>()
-
-        val steps = if (item == null) 0 else Util.computeSteps(item)
-        entries.add(PieEntry(steps.toFloat(), "current steps"))
-        val goal = if (steps > GOAL_STEPS) 0 else GOAL_STEPS - steps
-        entries.add(PieEntry(goal.toFloat(), "steps to reach for goal"))
-
-        val datasets = PieDataSet(entries, "steps")
-        datasets.colors = ColorTemplate.VORDIPLOM_COLORS.toList()
-        datasets.sliceSpace = 2f
-        datasets.valueTextColor = Color.WHITE
-        datasets.valueTextSize = 24f
-        return PieData(datasets)
     }
 
     companion object {
