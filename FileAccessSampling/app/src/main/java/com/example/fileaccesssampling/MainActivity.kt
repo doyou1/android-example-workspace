4fcfa1970376854e08f745517bde00491af508c9
doyou1 <tla149@daum.net>
2023-05-02 18:39:47 +0900

    file access sampling init

---
+++ FileAccessSampling/app/src/main/java/com/example/fileaccesssampling/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.fileaccesssampling
+
+import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
+import android.os.Bundle
+import android.provider.OpenableColumns
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.fileaccesssampling.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnGet.setOnClickListener {
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+            intent.addCategory(Intent.CATEGORY_OPENABLE)
+            intent.type = "*/*"
+            startActivityForResult(intent, READ_REQUEST_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == READ_REQUEST_CODE && resultCode == RESULT_OK) {
+            if (data != null) {
+                val uri: Uri? = data.data
+                if (uri != null) {
+                    Log.e("uri", "${getFileName(uri)}...")
+                }
+            }
+        }
+    }
+
+    fun getFileName(uri: Uri): String? {
+        var result: String? = null
+        if (uri.scheme == "content") {
+            val cursor: Cursor? = contentResolver.query(uri, null, null, null, null)
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                }
+            } finally {
+                if (cursor != null) {
+                    cursor.close()
+                }
+            }
+        }
+        if (result == null) {
+            result = uri.lastPathSegment
+        }
+        return result
+    }
+
+    companion object {
+        const val READ_REQUEST_CODE = 1
+    }
+}
